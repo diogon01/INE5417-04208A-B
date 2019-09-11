bf58dccb47b1cbae376419bd5481a5cdc6baaf29
Diogo Henrique <diogo.henrique.df@gmail.com>
2019-09-11 09:11:20 -0300

    Ajuste feito na Main, Retirado pacotes desnecessarios

--- rougue1/src/diablo2d/Main.java
+++ rougue1/src/diablo2d/Main.java
@@ -1,12 +1,11 @@
 package diablo2d;
 
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
 import dominioProblema.GameLogic;
 import interfaceUsuario.InterfaceJogo;
-import util.Recurso;
+
 
 public class Main extends JFrame {
 
@@ -17,23 +16,9 @@ public class Main extends JFrame {
 	private static InterfaceJogo interfaceJogo;
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-					Recurso.init();
-					interfaceJogo = new InterfaceJogo();
-					interfaceJogo.frame.setVisible(true);
-					GameLogic.escutarTeclado(interfaceJogo.frame);
-					
-				}catch (Exception e) {
-					e.printStackTrace();
-					System.out.println("[ERRO]"+ e.toString());
-				}
-				
-			}
-		});
+		interfaceJogo = new InterfaceJogo();
+		interfaceJogo.frame.setVisible(true);
+		GameLogic.escutarTeclado(interfaceJogo.frame);
 	}
 //		Recurso.init();
 //		Janela.criarJanela();
