9ce5386958f04b099870d3282f71ffdaea2fd6f3
diogo <diogo@DESKTOP-IT1GP9B>
2019-11-23 16:06:01 -0300

    Limpado a classe escutar caverna

--- rougue1/src/dominioProblema/EscutarCaverna.java
+++ rougue1/src/dominioProblema/EscutarCaverna.java
@@ -13,40 +13,11 @@ import util.Recurso;
 
 public class EscutarCaverna {
 
-	private static Timer tempo;
 
-	private static EntidadeBase jogador;
-
-	private static Level levelAtual;
-
-	/**
-	 * Chamado pelo Man quando o jogo *e iniciado
-	 */
 	public static void iniciarJogo() {
 
-		jogador = new EntidadeBase("jogador", 64, 64);
-
-		levelAtual = new Level(new String[] { "##########", "#...#....#", "#...#....#", "##.##....#", "#...#....#",
-				"#........#", "#...#....#", "##########" });
-
-		tempo = new Timer(200, new GameLoop());
-		tempo.start();
-
 	}
 
-	/**
-	 * Altera a posicao do jogadores
-	 * 
-	 * @param dirX Move o jogador no eixo X
-	 * @param dirY Move o jogador no eixo Y
-	 */
-	public static void moverJogador(int dirX, int dirY) {
-		jogador.setPosition(jogador.getPosX() + dirX, jogador.getPosY() + dirY);
-	}
-
-	public static EntidadeBase getJogador() {
-		return jogador;
-	}
 	
 	public static void escutarMouse(JFrame caverna) {
 		
